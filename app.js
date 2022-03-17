f58b7f2415bcd6cb374c0d122f88f50ffd819699
Corey McCoy <coreyamccoy@gmail.com>
2022-03-17 00:37:20 -0500

    Refactor

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const qrCodeContainer = document.querySelector('#qrcode-container');
 const qrCode = document.querySelector('#qrcode');
 const btn = document.querySelector('#button');
 const resetBtn = document.querySelector('#reset-button');
+const downloadQrCode = document.querySelector('.instruction-text');
 
 // Generate qr code
 function generateQRCode() {
@@ -12,8 +13,8 @@ function generateQRCode() {
     qrcodeContainer.innerHTML = '';
     new QRCode(qrcodeContainer, {
       text: website,
-      width: 128,
-      height: 128,
+      width: 200,
+      height: 200,
       colorDark: qrColor.value,
       colorLight: 'rgba(255,255,255,0)',
       correctLevel: QRCode.CorrectLevel.H,
@@ -23,10 +24,25 @@ function generateQRCode() {
     alert('Please enter a valid URL');
   }
 }
+// download qr code
+downloadQrCode.addEventListener('click', () => {
+  if (!input.value) {
+    alert('Please enter a valid URL');
+  } else {
+    let downloadLink = document.createElement('a');
+    downloadLink.href = document.querySelector('#qrcode-container canvas').toDataURL();
+    downloadLink.download = 'qrcode.png';
+    downloadLink.click();
+  }
+});
 
 // Reload the page
 function reset() {
-  location.reload();
+  if (!input.value) {
+    alert('Please enter a valid URL');
+  } else {
+    location.reload();
+  }
 }
 
 resetBtn.addEventListener('click', reset);
